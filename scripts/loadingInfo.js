4ad8d74b4f86027b10d85af03c15f5462c1e264d
nongdam <126311854+nongdam@users.noreply.github.com>
2024-05-31 20:33:50 +0900

    Update main.html foodInformation_base.html
    메인 페이지에서 정보 불러오기 성공

---
+++ scripts/loadingInfo.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Fetching data from:", '/data/foodsList.json');
+    fetch('/data/foodsList.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        } else {
+            return response.json();
+        }
+    })
+    .then(data => {
+        const container = document.querySelector('.foodList');
+        data.recipes.forEach(recipe => {
+            const defaultImage = '../Assets/placeholder-image.png'; // 기본 이미지 설정
+            const imageUrl = recipe.image || defaultImage; // 이미지 경로가 없는 경우 기본 이미지 사용
+
+            const recipeElement = document.createElement('li');
+            recipeElement.innerHTML = `
+                <form action="./foodInformation_base.html" method="get">
+                    <button type="submit" class="foodCard">
+                        <img src="${imageUrl}" alt="${recipe.name}" class="recommend">
+                        <input type="hidden" name="foodName" value="${recipe.name}">
+                        <input type="hidden" name="foodTag" value="${recipe.category}">
+                        <input type="hidden" name="image" value="${imageUrl}"> <!-- 이미지 경로 추가 -->
+                        <input type="hidden" name="ingredients" value="${recipe.ingredients.join(',')}">
+                        <input type="hidden" name="method" value="${recipe.method.join('. ')}">
+                        <input type="hidden" name="calories" value="${recipe.calories}">
+                        <input type="hidden" name="nutrients" value="${recipe.nutrients.join(',')}">
+                        <h1>${recipe.name}</h1>
+                        <h3>${recipe.category}</h3>
+                    </button>
+                </form>
+            `;
+            container.appendChild(recipeElement);
+        });
+    })
+    .catch(error => {
+        console.error('Error loading the recipes:', error);
+    });
+});
